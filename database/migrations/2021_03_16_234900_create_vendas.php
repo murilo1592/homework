50b64c6f3a44163ac999f0ee52a3f7674134a716
Murilo Mattos <murilomattos>
2021-03-16 21:03:28 -0300

    HomeWork

---
+++ database/migrations/2021_03_16_234900_create_vendas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('vendedor_id')
+                ->references('id')
+                ->on('vendedores')
+                ->onDelete('cascade');
+            $table->float('valor_venda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+}
